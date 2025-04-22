1465ed8fdf31cbc2bbc634d2e1510d58688f62c7
sharanu dhammur <150852531+sharanudhammur@users.noreply.github.com>
2025-04-22 12:46:23 +0530

    Delete src directory

--- src/app/gallery/page.jsx
+++
@@ -1,8 +0,0 @@
-import { getMetaInfo } from '@/utils/metaInfo';
-import CompanyGallery from './companyGallery';
-
-export const metadata = getMetaInfo("Gallery");
-
-export default function GalleryPage() {
-  return <CompanyGallery />;
-}
